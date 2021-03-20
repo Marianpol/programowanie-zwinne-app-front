693d37fe48dcb34f124058c82549ce0d376549b3
Marianpol <marcin.musial20@gmail.com>
2021-03-20 21:33:28 +0100

    Add button and input style

---
+++ pages/register.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Register = () => {
+
+    return (
+        <div>
+            <div>
+                <p>Rejestracja</p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Register;
