aad888bacc9eb88c7549ab42db934af21d897b9e
Marianpol <marcin.musial20@gmail.com>
2021-03-21 11:06:48 +0100

    Layout switch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import Layout from "app/layout/components/Layout";
 import React from "react";
+import { useRouter } from 'next/router';
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: any) {
+
+  const { pathname } = useRouter();
+  
+  if (['/login', '/register'].includes(pathname))
+    return <Component {...pageProps}/>
+
   return (
     <Layout>
       <Component {...pageProps} />
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from '../components/Login';
+import Login from 'app/layout/components/Login';
 
 const login = () => {
 
